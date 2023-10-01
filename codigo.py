4945dab3bb9c7016b2d60388f1c11089f1b4213c
ValNicolini <135516222+ValNicolini@users.noreply.github.com>
2023-09-30 21:35:15 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -10,26 +10,47 @@
 import flet as ft
 
 def main(pagina):
-    texto = ft.Text('Val Nicolini')
+    texto = ft.Text('Bate-Papo')
+    
+    text = ft.Text('Entrou no chat')
+    chat = ft.Column()
 
     nome_usuario = ft.TextField(label='Escreva seu nome')
 
+    def enviar_mensagem_tunel(mensagem):
+       #adicionar a mensagem no chat
+       chat.controls.append(ft.Text(mensagem))
+       pagina.update()
+       
+       #túnel de mensagem
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+      
+    def enviar_mensagem(evento):
+       pagina.pubsub.send_all(campo_mensagem.value)
+       #limpar o campo de mensagem
+       campo_mensagem.value=''
+       pagina.remove(text)
+       pagina.update()
+    
     campo_mensagem = ft.TextField(label='Digite sua mensagem')
 
-    botao_enviar_mensagem = ft.ElevatedButton('Enviar')
+    botao_enviar_mensagem = ft.ElevatedButton('Enviar', on_click=enviar_mensagem)
     
     def entrar_popup(evento):
-       popup.open=True,
-       pagina.remove(botao_iniciar),
-       pagina.add(campo_mensagem),
-       pagina.add(botao_enviar_mensagem),
-       pagina.remove(texto),
-
+       pagina.add(text)
+       #adicionar o chat
+       pagina.add(chat)
+       #fechar popup
+       popup.open=False
+       #remover botão iniciar chat
+       pagina.remove(botao_iniciar)
+       pagina.remove(texto)
+       #adicionar campo de mensagem do usuário
+       #adicionar botão de enviar mensagem
+       pagina.add(ft.Row([campo_mensagem, botao_enviar_mensagem])),
        pagina.update()
     
        
-       
-
     popup = ft.AlertDialog(
        open=False,
        modal=True,
@@ -42,13 +63,11 @@ def main(pagina):
      pagina.dialog = popup
      popup.open=True
      pagina.update()
-
-     texto = ft.Text('Entrou no chat')
-     pagina.add(texto)
         
 
     botao_iniciar = ft.ElevatedButton('Iniciar Chat', on_click=entrar_chat)
-
+    
+    pagina.add(texto)
     pagina.add(botao_iniciar)
     
     
